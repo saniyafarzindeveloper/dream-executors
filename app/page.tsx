18a869b6f6bd17f3e62b0d355ce8c6f92d174426
saniyafarzindeveloper <developerfarzin@gmail.com>
2026-03-31 16:34:11 +0530

    installed shadcn UI, added layout components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Page = () => {
-  return <div className="">Page</div>;
+  return <div className="">
+    <Button>
+      Hello
+    </Button>
+  </div>;
 };
 
 export default Page;
